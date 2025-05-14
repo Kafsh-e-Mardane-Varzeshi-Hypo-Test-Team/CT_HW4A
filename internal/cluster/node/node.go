0f993b73c3156f9bc1d37c4fd26e523bd55a201d
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-05-15 00:16:26 +0330

    fix: undo temp commit

--- internal/cluster/node/node.go
+++ internal/cluster/node/node.go
@@ -1,22 +1,56 @@
 package node
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Kafsh-e-Mardane-Varzeshi-Hypo-Test-Team/CT_HW3/internal/cluster/replica"
+)
+
 type Node struct {
-	// TODO
+	Id       int
+	replicas map[int]replica.Replica // partitionId, replica of that partiotionId
 }
 
-func (n *Node) Set(key, value string) (NodeSetResp, error) {
-	// TODO
-	return NodeSetResp{}, nil
+type NodeResp struct {
+	partitionId int
+	// enum ok, nok
+	// error
+	replica.ReplicaResp
 }
 
-func (n *Node) Get(key string) (NodeGetResp, error) {
-	// TODO
-	return NodeGetResp{}, nil
+func NewNode(id int) Node {
+	return Node{
+		Id:       id,
+		replicas: make(map[int]replica.Replica),
+	}
 }
 
-func (n *Node) Delete(key string) (NodeDeleteResp, error) {
+func (n *Node) Set(key, value string) (NodeResp, error) {
 	// TODO
-	return NodeDeleteResp{}, nil
+	replica, ok := n.replicas[getReplicaId(key)]
+	if !ok {
+		return errors.New(fmt.Sprintf("node id: %v contains no partition containing key:%s", n.Id, key))
+	}
+
+	replicaResp, err := replica.Set(key, value)
+	if err != nil {
+		return err
+	}
+	if replica.isLeader {
+		return broadcastSet(replicaResp)
+	}
+	return nil
+}
+
+func (n *Node) Get(key string) (NodeResp, error) {
+	// TODO
+	return NodeResp{}, nil
+}
+
+func (n *Node) Delete(key string) (NodeResp, error) {
+	// TODO
+	return NodeResp{}, nil
 }
 
 func Start() {
@@ -36,4 +70,18 @@ func tcpInitialization() {
 
 func heartbeat() {
 	// TODO
+}
+
+func getReplicaId(key string) int {
+	// TODO
+	return 5
+}
+
+func broadcastSet(replicaResp replica.ReplicaResp) error {
+	// TODO
+	GetPartition(id)
+	{
+		nodes []string
+	}
+	return nil
 }
