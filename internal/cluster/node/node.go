61d78d764cdd51efb5e1fb5b5e40c7e54013e689
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-05-17 17:12:29 +0330

    feat: implement SetInLeaderReplica method

--- internal/cluster/node/node.go
+++ internal/cluster/node/node.go
@@ -1,7 +1,6 @@
 package node
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/Kafsh-e-Mardane-Varzeshi-Hypo-Test-Team/CT_HW3/internal/cluster/replica"
@@ -9,48 +8,53 @@ import (
 
 type Node struct {
 	Id       int
-	replicas map[int]replica.Replica // partitionId, replica of that partiotionId
-}
-
-type NodeResp struct {
-	partitionId int
-	// enum ok, nok
-	// error
-	replica.ReplicaResp
+	replicas map[int]*replica.LeaderReplica // partitionId, replica of that partiotionId
 }
 
 func NewNode(id int) Node {
 	return Node{
 		Id:       id,
-		replicas: make(map[int]replica.Replica),
+		replicas: make(map[int]*replica.LeaderReplica),
 	}
 }
 
-func (n *Node) Set(key, value string) (NodeResp, error) {
-	// TODO
-	replica, ok := n.replicas[getReplicaId(key)]
+func (n *Node) SetInLeaderReplica(partiotionId int, key, value string) error {
+	// find the replica that has to store this key
+	replica, ok := n.replicas[partiotionId]
 	if !ok {
-		return errors.New(fmt.Sprintf("node id: %v contains no partition containing key:%s", n.Id, key))
+		return fmt.Errorf("node id: %v contains no partition containing key:%s", n.Id, key)
 	}
 
-	replicaResp, err := replica.Set(key, value)
+	// if we have only one replica struct (not leader and follower):
+	// if !partition.IsLeader {
+	//    return fmt.Errorf("node id: %v contains no leader for partition %v", n.Id, partitionID)
+	// }
+
+	// TODO: append to WAL
+
+	// set key, value
+	replicaLog, err := replica.Set(key, value)
 	if err != nil {
 		return err
 	}
-	if replica.isLeader {
-		return broadcastSet(replicaResp)
+
+	// TODO: broadcast to followers
+	err = n.broadcastToFollowers(replicaLog)
+	if err != nil {
+		return err
 	}
+
 	return nil
 }
 
-func (n *Node) Get(key string) (NodeResp, error) {
+func (n *Node) Get(key string) error {
 	// TODO
-	return NodeResp{}, nil
+	return nil
 }
 
-func (n *Node) Delete(key string) (NodeResp, error) {
+func (n *Node) Delete(key string) error {
 	// TODO
-	return NodeResp{}, nil
+	return nil
 }
 
 func Start() {
@@ -72,16 +76,7 @@ func heartbeat() {
 	// TODO
 }
 
-func getReplicaId(key string) int {
+func (n *Node) broadcastToFollowers(replicaLog replica.ReplicaLog) error {
 	// TODO
-	return 5
-}
-
-func broadcastSet(replicaResp replica.ReplicaResp) error {
-	// TODO
-	GetPartition(id)
-	{
-		nodes []string
-	}
 	return nil
 }
