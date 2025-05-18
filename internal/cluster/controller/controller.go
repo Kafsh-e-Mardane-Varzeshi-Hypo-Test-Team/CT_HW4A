69459db6eddbf8e26abd2cd11c795a1dee707c34
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-05-18 15:04:11 +0330

    fix: remove redunant method from controller

--- internal/cluster/controller/controller.go
+++ internal/cluster/controller/controller.go
@@ -3,9 +3,3 @@ package controller
 type Controller struct {
 	// TODO
 }
-
-func GetNodesContainingPartition(partiotionId int) []*NodeMetadata {
-	// TODO
-	nodes := make([]*NodeMetadata, 5)
-	return nodes
-}
