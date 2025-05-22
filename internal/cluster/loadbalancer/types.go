7898dbbd1870d5d21ca91cde8f640c0c7cbf4ce8
mammedbrk <mammedbrk@gmail.com>
2025-05-22 17:23:50 +0330

    feat: implement load balancer methods

--- internal/cluster/loadbalancer/types.go
+++ internal/cluster/loadbalancer/types.go
@@ -1,5 +1,11 @@
 package loadbalancer
 
+import "github.com/Kafsh-e-Mardane-Varzeshi-Hypo-Test-Team/CT_HW3/internal/cluster/controller"
+
+type PartitionMetadataList struct {
+	partitions []*controller.PartitionMetadata
+}
+
 type LBSetResp struct {
 	// TODO
 }
