e4edd635ef7d39a0890e6fda018d9f665015f587
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-05-23 11:39:05 +0330

    feat: implement handleDeletePartition in node handler

--- internal/cluster/node/handler.go
+++ internal/cluster/node/handler.go
@@ -11,7 +11,7 @@ import (
 
 func (n *Node) setupRoutes() {
 	n.ginEngine.POST("/add-partition/:partition-id", n.handleAddPartition)
-	// n.ginEngine.POST("/metadata", n.handleSendPartition)
+	n.ginEngine.POST("/delete-partition/:partition-id", n.handleDeletePartition)
 	// n.ginEngine.DELETE("/delete-partition", n.handleDeletePartition)
 }
 
@@ -28,13 +28,27 @@ func (n *Node) handleAddPartition(c *gin.Context) {
 		c.JSON(http.StatusConflict, gin.H{"error": "this partitionId already exists"})
 		return
 	}
-	
+
 	n.replicas[partitionId] = replica.NewReplica(5, n.Id, partitionId, replica.Leader)
 	c.JSON(http.StatusOK, nil)
 }
 
-func (n *Node) handleDeletePartition() {
-	// TODO
+func (n *Node) handleDeletePartition(c *gin.Context) {
+	partitionId, err := strconv.Atoi(c.Param("partition-id"))
+	if err != nil {
+		log.Printf("[node.handleDeletePartition] error while converting partitionId param to int: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if _, ok := n.replicas[partitionId]; !ok {
+		log.Printf("[node.handleDeletePartition] partitionId %v does not exist in nodeId %v", partitionId, n.Id)
+		c.JSON(http.StatusNotFound, gin.H{"error": "this partitionId does not exist"})
+		return
+	}
+
+	n.replicas[partitionId] = nil
+	c.JSON(http.StatusOK, nil)
 }
 
 func (n *Node) handleSendPartition() {
