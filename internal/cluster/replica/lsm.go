661b735be4d6443e7fe975395d82b3d6bdf787dc
Mefi <Mefi.Taheri@gmail.com>
2025-05-23 23:53:40 +0330

    feat: add wal to lsm in replica

--- internal/cluster/replica/lsm.go
+++ internal/cluster/replica/lsm.go
@@ -1,6 +1,9 @@
 package replica
 
-import "sync"
+import (
+	"sort"
+	"sync"
+)
 
 const maxLen int = 1e4
 
@@ -16,6 +19,7 @@ type LSM struct {
 	mu         sync.RWMutex
 	mem        *MemTable
 	immutables []*MemTable
+	wal        []ReplicaLog
 }
 
 func NewMemTable() *MemTable {
@@ -29,12 +33,14 @@ func NewLSM() *LSM {
 		mu:         sync.RWMutex{},
 		mem:        NewMemTable(),
 		immutables: []*MemTable{},
+		wal:        []ReplicaLog{},
 	}
 }
 
 func (l *LSM) AddLogEntry(logEntry ReplicaLog) {
 	l.mu.Lock()
 	l.mem.data = append(l.mem.data, logEntry)
+	l.wal = append(l.wal, logEntry)
 	l.mu.Unlock()
 
 	if len(l.mem.data) >= maxLen {
@@ -52,4 +58,14 @@ func (l *LSM) flush() {
 
 	l.immutables = append(l.immutables, l.mem)
 	l.mem = NewMemTable()
+}
+
+func (l *LSM) WALAfter(ts int64) []ReplicaLog {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+
+	idx := sort.Search(len(l.wal), func(i int) bool {
+		return !(l.wal[i].Timestamp < ts)
+	})
+	return l.wal[idx:] // return a copy
 }
