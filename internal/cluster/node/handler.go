c1b3b7002a615b6eb8c747e90636ea73ddbb7ae0
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-05-23 11:35:46 +0330

    feat: implement handleAddPartition in node handler

---
+++ internal/cluster/node/handler.go
@@ -0,0 +1,42 @@
+package node
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Kafsh-e-Mardane-Varzeshi-Hypo-Test-Team/CT_HW3/internal/cluster/replica"
+	"github.com/gin-gonic/gin"
+)
+
+func (n *Node) setupRoutes() {
+	n.ginEngine.POST("/add-partition/:partition-id", n.handleAddPartition)
+	// n.ginEngine.POST("/metadata", n.handleSendPartition)
+	// n.ginEngine.DELETE("/delete-partition", n.handleDeletePartition)
+}
+
+func (n *Node) handleAddPartition(c *gin.Context) {
+	partitionId, err := strconv.Atoi(c.Param("partition-id"))
+	if err != nil {
+		log.Printf("[node.handleAddPartition] error while converting partitionId param to int: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if _, ok := n.replicas[partitionId]; ok {
+		log.Printf("[node.handleAddPartition] partitionId %v already exists in nodeId %v", partitionId, n.Id)
+		c.JSON(http.StatusConflict, gin.H{"error": "this partitionId already exists"})
+		return
+	}
+	
+	n.replicas[partitionId] = replica.NewReplica(5, n.Id, partitionId, replica.Leader)
+	c.JSON(http.StatusOK, nil)
+}
+
+func (n *Node) handleDeletePartition() {
+	// TODO
+}
+
+func (n *Node) handleSendPartition() {
+	// TODO
+}
